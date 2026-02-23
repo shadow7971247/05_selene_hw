0708d623e04963e7047ef1a9d9bbca19ed3b6c0b
Artur Gizatullin <jhonconstantine1996@gmail.com>
2026-02-23 20:12:06 +0500

    Внес правки по дз, файл conftest.py стал уродливее и сложнее из за проблемы со всплывающим окном сохранения адреса в хроме. Пришлось подгружать существующий профиль с выставленными настройками и подгружать selenium

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,12 @@ from selenium.webdriver.chrome.service import Service
 @pytest.fixture(autouse=True)
 def browser_management():
     service = Service(executable_path='C:/webdriver/chromedriver.exe')
-
     options = webdriver.ChromeOptions()
-
     options.add_argument('--user-data-dir=C:\\temp\\chrome_test_profile')
     options.add_argument('--disable-save-password-bubble')
     options.add_argument('--disable-notifications')
     options.add_argument('--start-maximized')
-
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
-
     driver = webdriver.Chrome(service=service, options=options)
     browser.config.driver = driver
     browser.config.timeout = 10
