f72de082126d665a2d8ed5fddc9bcbd96bda898c
Artur Gizatullin <jhonconstantine1996@gmail.com>
2026-02-23 20:15:45 +0500

    Внес правки по дз, файл conftest.py стал уродливее и сложнее из за проблемы со всплывающим окном сохранения адреса в хроме. Пришлось подгружать существующий профиль с выставленными настройками и подгружать selenium

--- tests/test_form.py
+++ tests/test_form.py
@@ -12,8 +12,8 @@ def test_fill_form():
     browser.element('//input[@value="Male"]').click()
     browser.element("#userNumber").type("1234567890")
 
-    #так и не понял как сделать мануальный выбор
-    browser.element('#dateOfBirthInput').clear().type('13-11-1995')
+    # так и не понял как сделать мануальный выбор
+    browser.element("#dateOfBirthInput").clear().type("13-11-1995")
 
     browser.element('//input[@id="hobbies-Music"]').click()
     browser.element('//input[@id="hobbies-Reading"]').click()
@@ -27,11 +27,27 @@ def test_fill_form():
     # Отправить и проверить
     browser.element("#submit").click()
     browser.element('//*[contains(text(), "Thanks for submitting")]').should(be.visible)
-    browser.element('//td[text()="Student Name"]/following-sibling::td').should(have.text('Кто то там Такой то'))
-    browser.element('//td[text()="Student Email"]/following-sibling::td').should(have.text('ktoto@example.com'))
-    browser.element('//td[text()="Gender"]/following-sibling::td').should(have.text('Male'))
-    browser.element('//td[text()="Mobile"]/following-sibling::td').should(have.text('1234567890'))
-    browser.element('//td[text()="Date of Birth"]/following-sibling::td').should(have.text('1995-11-13'))
-    browser.element('//td[text()="Hobbies"]/following-sibling::td').should(have.text('Music, Reading'))
-    browser.element('//td[text()="Address"]/following-sibling::td').should(have.text('Улица Пушкина'))
-    browser.element('//td[text()="State and City"]/following-sibling::td').should(have.text('NCR Delhi'))
+    browser.element('//td[text()="Student Name"]/following-sibling::td').should(
+        have.text("Кто то там Такой то")
+    )
+    browser.element('//td[text()="Student Email"]/following-sibling::td').should(
+        have.text("ktoto@example.com")
+    )
+    browser.element('//td[text()="Gender"]/following-sibling::td').should(
+        have.text("Male")
+    )
+    browser.element('//td[text()="Mobile"]/following-sibling::td').should(
+        have.text("1234567890")
+    )
+    browser.element('//td[text()="Date of Birth"]/following-sibling::td').should(
+        have.text("1995-11-13")
+    )
+    browser.element('//td[text()="Hobbies"]/following-sibling::td').should(
+        have.text("Music, Reading")
+    )
+    browser.element('//td[text()="Address"]/following-sibling::td').should(
+        have.text("Улица Пушкина")
+    )
+    browser.element('//td[text()="State and City"]/following-sibling::td').should(
+        have.text("NCR Delhi")
+    )
